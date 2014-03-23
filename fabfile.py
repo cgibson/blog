c92e63becf56be97e6b63dcc3fd772810e71e217
Chris Gibson <cgibson@mrvoxel.com>
2014-03-23 01:15:35 -0700

    added fabfile for better deploys

---
+++ fabfile.py
@@ -0,0 +1,48 @@
+from fabric.api import task
+from fabric.context_managers import cd, prefix
+from fabric.operations import run
+from fabric.contrib.files import exists
+
+github_repo_url = "https://github.com/cgibson/blog"
+
+pelican_virtualenv = "~/env/pelican"
+
+remote_repo_dir = "~/Projects/blog"
+
+remote_beta_dir = "/sites/mrvoxel/beta"
+remote_beta_redir_file = "/sites/mrvoxel/site-map-beta.lua"
+
+remote_publish_dir = "/sites/mrvoxel/www"
+remote_publish_redir_file = "/sites/mrvoxel/site-map.lua"
+
+def title(text):
+    print "=" * 80
+    print " " + str(text)
+    print "=" * 80
+
+@task
+def deploy():
+    
+    title("Deploying to beta server.")
+
+    if not exists(remote_repo_dir):
+        print "Repo dir doesn't exist. Building."
+        run("mkdir -p %s" % remote_repo_dir)
+        run("git clone %s %s" % (github_repo_url, remote_repo_dir))
+
+    with cd(remote_repo_dir):
+        run("git pull")
+
+        run("git submodule update --init --recursive")
+        with cd("theme"):
+            run("git checkout master")
+            run("git pull")
+
+
+        # Now build the site
+        with prefix("source %s/bin/activate" % pelican_virtualenv):
+            run("make publish")
+
+        # Copy generated files to their respective spots
+        run("cp -r %s/output %s" % (remote_repo_dir, remote_beta_dir))
+        run("cp %s/output/site-map.lua %s" % (remote_repo_dir, remote_beta_redir_file))
